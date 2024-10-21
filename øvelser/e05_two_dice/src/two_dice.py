5b633f562456e26c4ddbfa6cced29ccb929cd939
LobaDK <gta5.r6s.bf4@gmail.com>
2024-10-21 11:23:21 +0200

    completed test 5

--- øvelser/e05_two_dice/src/two_dice.py
+++ øvelser/e05_two_dice/src/two_dice.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
-def main():
-    pass
+
+def main() -> None:
+    dice_1 = range(1, 7)
+    dice_2 = range(1, 7)
+    expected_sum = 5
+
+    for i in dice_1:
+        for j in dice_2:
+            if i + j == expected_sum:
+                print(f"({i},{j})")
+
 
 if __name__ == "__main__":
     main()
