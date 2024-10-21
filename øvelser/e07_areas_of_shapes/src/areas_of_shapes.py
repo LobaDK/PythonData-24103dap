b5b8485f2a6e2813c7941e21a89a6ce70c50c2c1
LobaDK <gta5.r6s.bf4@gmail.com>
2024-10-21 12:33:19 +0200

    Complete test 7

--- øvelser/e07_areas_of_shapes/src/areas_of_shapes.py
+++ øvelser/e07_areas_of_shapes/src/areas_of_shapes.py
@@ -2,9 +2,47 @@
 
 import math
 
+shapes: list[str] = ["triangle", "rectangle", "circle"]
 
-def main():
-    # enter you solution here
+
+def calculate_area_of_triangle(base: float, height: float) -> float:
+    return 0.5 * base * height
+
+
+def calculate_area_of_rectangle(height: float, width: float) -> float:
+    return height * width
+
+
+def calculate_area_of_circle(radius: float) -> float:
+    return math.pi * math.pow(radius, 2)
+
+
+def main() -> None:
+    while True:
+        chosen_shape: str = input(f"Choose a shape ({", ".join(shapes)}): ").lower()
+        if not chosen_shape:
+            break
+
+        if chosen_shape not in shapes:
+            print("Unknown shape!")
+            continue
+
+        if chosen_shape == "triangle":
+            tri_base = float(input("Give base of the triangle: "))
+            tri_height = float(input("Give height of the triangle: "))
+            area: float = calculate_area_of_triangle(base=tri_base, height=tri_height)
+
+        elif chosen_shape == "rectangle":
+            rect_width = float(input("Give width of the rectangle: "))
+            rect_height = float(input("Give height of the rectangle: "))
+            area = calculate_area_of_rectangle(rect_height, rect_width)
+
+        elif chosen_shape == "circle":
+            circ_radius = float(input("Give radius of the circle: "))
+            area = calculate_area_of_circle(circ_radius)
+
+        print(f"The area is {area}")
+
 
 if __name__ == "__main__":
     main()
