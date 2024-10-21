208bd0acd6182d6137c5090fe03df8b5e955382e
LobaDK <gta5.r6s.bf4@gmail.com>
2024-10-21 12:03:47 +0200

    Completed test 6

--- øvelser/e06_triple_square/src/triple_square.py
+++ øvelser/e06_triple_square/src/triple_square.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 
 
-def main():
-    pass
+def triple(n: int) -> int:
+    return n * 3
+
+
+def square(n: int) -> int:
+    return n**2
+
+
+def main() -> None:
+    for i in range(1, 11):
+        _triple = triple(i)
+        _square = square(i)
+        if _square > _triple:
+            break
+        print(f"triple({i})=={_triple} square({i})=={_square}")
+
 
 if __name__ == "__main__":
     main()
