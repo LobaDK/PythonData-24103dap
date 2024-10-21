095f66db41d1a0d26ce779e400a3f738f178d5b5
LobaDK <gta5.r6s.bf4@gmail.com>
2024-10-21 12:53:24 +0200

    Complete test 8

--- øvelser/e08_solve_quadratic/src/solve_quadratic.py
+++ øvelser/e08_solve_quadratic/src/solve_quadratic.py
@@ -2,12 +2,16 @@
 
 import math
 
+
 def solve_quadratic(a, b, c):
-    return (0,0)
+    d = b**2 - 4 * a * c
+    return (-b + math.sqrt(d)) / (2 * a), (-b - math.sqrt(d)) / (2 * a)
 
 
 def main():
-    pass
+    print(solve_quadratic(1, -3, 2))
+    print(solve_quadratic(1, 2, 1))
+
 
 if __name__ == "__main__":
     main()
