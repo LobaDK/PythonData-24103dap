f827b4681a149b582e0a57bdd667006104c28d03
LobaDK <gta5.r6s.bf4@gmail.com>
2024-10-21 10:43:02 +0200

    Completed test 3

--- øvelser/e03_multiplication/src/multiplication.py
+++ øvelser/e03_multiplication/src/multiplication.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
-def main():
-    # Enter your solution here
+
+def main() -> None:
+    number_to_multiply: int = 4
+    for i in range(11):
+        result: int = number_to_multiply * i
+        print(f"{number_to_multiply} multiplied by {i} is {result}")
+
 
 if __name__ == "__main__":
     main()
