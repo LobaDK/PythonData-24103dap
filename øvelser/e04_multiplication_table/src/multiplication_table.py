7d7139b959d21c0870d27bbf25e26777670349ea
LobaDK <gta5.r6s.bf4@gmail.com>
2024-10-21 11:15:41 +0200

    complete test 4

--- øvelser/e04_multiplication_table/src/multiplication_table.py
+++ øvelser/e04_multiplication_table/src/multiplication_table.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 
-def main():
-    pass
+def main() -> None:
+    for i in range(1, 11):
+        print(" ".join([f"{str(object=i * j): >3}" for j in range(1, 11)]))
+
 
 if __name__ == "__main__":
     main()
