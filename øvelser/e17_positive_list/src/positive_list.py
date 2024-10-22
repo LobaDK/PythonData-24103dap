221f5573253abf0d07ef86a988836382542cc446
LobaDK <gta5.r6s.bf4@gmail.com>
2024-10-22 12:13:14 +0200

    Complete test 17

--- øvelser/e17_positive_list/src/positive_list.py
+++ øvelser/e17_positive_list/src/positive_list.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
-def positive_list(L):
-    return []
 
-def main():
-    pass
+def positive_list(L: list[int]) -> list[int]:
+    return list(filter(lambda x: x > 0, L))
+
+
+def main() -> None:
+    test_list: list[int] = [2, -2, 0, 1, -7]
+    print(positive_list(L=test_list))
+
 
 if __name__ == "__main__":
     main()
