61860c3519607145d9ba7181236384e30d461a67
LobaDK <gta5.r6s.bf4@gmail.com>
2024-10-22 12:53:43 +0200

    Complete test 11

--- øvelser/e11_interleave/src/interleave.py
+++ øvelser/e11_interleave/src/interleave.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
+
 def interleave(*lists):
-    return []
+    return [val for tup in zip(*lists) for val in tup]
+
 
 def main():
-    print(interleave([1, 2, 3], [20, 30, 40], ['a', 'b', 'c']))
+    print(interleave([1, 2, 3], [20, 30, 40], ["a", "b", "c"]))
+
 
 if __name__ == "__main__":
     main()
