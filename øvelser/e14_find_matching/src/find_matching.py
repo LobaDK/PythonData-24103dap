7b7db247cfd2452621045a054de0dba7dec35bd5
LobaDK <gta5.r6s.bf4@gmail.com>
2024-10-22 14:00:37 +0200

    Complete test 14

--- øvelser/e14_find_matching/src/find_matching.py
+++ øvelser/e14_find_matching/src/find_matching.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
-def find_matching(L, pattern):
-    return []
+
+def find_matching(L, pattern) -> list[int]:
+    matches: list[int] = []
+    for index, word in enumerate(iterable=L):
+        if pattern in word:
+            matches.append(index)
+
+    return matches
+
 
 def main():
-    pass
+    print(find_matching(L=["sensitive", "engine", "rubbish", "comment"], pattern="en"))
+
 
 if __name__ == "__main__":
     main()
