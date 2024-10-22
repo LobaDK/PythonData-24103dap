35623b5afcd2b8f686f7707219fe5679fad41e88
LobaDK <gta5.r6s.bf4@gmail.com>
2024-10-22 13:56:39 +0200

    Complete test 13

--- øvelser/e13_reverse_dictionary/src/reverse_dictionary.py
+++ øvelser/e13_reverse_dictionary/src/reverse_dictionary.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python3
 
-def reverse_dictionary(d):
-    return {}
 
-def main():
-    pass
+def reverse_dictionary(d: dict[str, list[str]]) -> dict[str, list[str]]:
+    rd: dict[str, list[str]] = {}
+    for key, value in d.items():
+        for w in value:
+            if w in rd:
+                rd[w] += [key]
+            else:
+                rd[w] = [key]
+
+    return rd
+
+
+def main() -> None:
+    d: dict[str, list[str]] = {
+        "move": ["liikuttaa"],
+        "hide": ["piilottaa", "salata"],
+        "six": ["kuusi"],
+        "fir": ["kuusi"],
+    }
+
+    print(reverse_dictionary(d=d))
+
 
 if __name__ == "__main__":
     main()
