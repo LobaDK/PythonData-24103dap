1c33017cd8e0bbcd0f53cd706380f4aaa2ca1ae5
LobaDK <gta5.r6s.bf4@gmail.com>
2024-10-23 13:10:41 +0200

    Complete test 28

--- øvelser/e28_prepend/src/prepend.py
+++ øvelser/e28_prepend/src/prepend.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
-class Prepend(object):
-    # Add the methods of the class here
 
-def main():
-    pass
+class Prepend(object):
+    def __init__(self, start: str) -> None:
+        self.start: str = start
+
+    def write(self, string: str) -> None:
+        print(self.start + string)
+
+
+def main() -> None:
+    test = Prepend(start=">>> ")
+    test.write(string="Python")
+
 
 if __name__ == "__main__":
     main()
