8fe0cac40206f563edbb38dd01931d3df8a728be
LobaDK <gta5.r6s.bf4@gmail.com>
2024-10-23 10:02:44 +0200

    Complete test 19

--- øvelser/e19_sum_equation/src/sum_equation.py
+++ øvelser/e19_sum_equation/src/sum_equation.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
-def sum_equation(L):
-    return ""
 
-def main():
-    pass
+def sum_equation(L: list[int]) -> str:
+    return " + ".join(map(str, L)) + f" = {sum(L)}" if L else "0 = 0"
+
+
+def main() -> None:
+    print(sum_equation([1, 5, 7]))
+
 
 if __name__ == "__main__":
     main()
