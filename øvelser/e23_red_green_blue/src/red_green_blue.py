aa6f09f9e5050036d2d81a57776770515e390d1a
LobaDK <gta5.r6s.bf4@gmail.com>
2024-10-23 12:13:14 +0200

    Complete test 22

--- øvelser/e23_red_green_blue/src/red_green_blue.py
+++ øvelser/e23_red_green_blue/src/red_green_blue.py
@@ -2,12 +2,25 @@
 
 import re
 
-def red_green_blue(filename="src/rgb.txt"):
-    return []
+PATTERN = r"(\d{1,3})\s+(\d{1,3})\s+(\d{1,3})\s+(.+)$"
 
 
-def main():
-    pass
+def red_green_blue(
+    filename="/home/lobadk/Documents/PythonData-24103dap/øvelser/e23_red_green_blue/src/rgb.txt",
+) -> list[str]:
+    with open(file=filename, mode="r") as read:
+        return [
+            f"{match[1]}\t{match[2]}\t{match[3]}\t{match[4]}"
+            for match in [
+                re.search(pattern=PATTERN, string=line) for line in read.readlines()
+            ]
+            if match
+        ]
+
+
+def main() -> None:
+    print(red_green_blue())
+
 
 if __name__ == "__main__":
     main()
