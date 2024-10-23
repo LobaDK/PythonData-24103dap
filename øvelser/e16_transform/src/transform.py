a65d7ea8246043c89c01a9d35146b93cac6ffab8
LobaDK <gta5.r6s.bf4@gmail.com>
2024-10-23 09:16:29 +0200

    Complete test 16

--- øvelser/e16_transform/src/transform.py
+++ øvelser/e16_transform/src/transform.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
-def transform(s1, s2):
-    return []
 
-def main():
-    pass
+def transform(s1: str, s2: str) -> list[int]:
+    return [
+        transformed
+        for transformed in map(
+            lambda x: int(x[0]) * int(x[1]), zip(s1.split(), s2.split())
+        )
+    ]
+
+
+def main() -> None:
+    print(transform(s1="1 5 3", s2="2 6 -1"))
+
 
 if __name__ == "__main__":
     main()
